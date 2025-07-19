059b1de355bcceb2f68c69827db29d1972ac2dc4
Mateus Chichera <mateeusz17@hotmail.com>
2025-07-19 12:47:20 -0300

    criado tela de lojas

--- src/controllers/configEmpresaController.js
+++ src/controllers/configEmpresaController.js
@@ -11,6 +11,7 @@ const getConfigBySlug = async (req, res, next) => {
           e.id, e.nome_fantasia, e.razao_social, e.cnpj, e.slug, e.email_contato, e.telefone_contato,
           e.endereco, e.cidade, e.estado, e.cep, e.observacoes, e.status, e.valor_mensalidade,
           e.data_vencimento_mensalidade, e.data_cadastro, e.data_atualizacao,
+          e.segmento,
           ce.logo_url, ce.horario_funcionamento, ce.numero_mesas, ce.taxa_entrega,
           ce.tempo_medio_preparo, ce.config_impressora,
           ce.permitir_pedido_online, ce.pedido_minimo_delivery, ce.desativar_entrega,
@@ -18,7 +19,7 @@ const getConfigBySlug = async (req, res, next) => {
           ce.auto_aprovar_pedidos, ce.cor_primaria_cardapio, ce.mostrar_promocoes_na_home,
           ce.layout_cardapio, ce.alerta_estoque_baixo_ativo, ce.limite_estoque_baixo,
           ce.enviar_email_confirmacao, ce.som_notificacao_cozinha, ce.som_notificacao_delivery,
-          ce.valor_inicial_caixa_padrao, ce.exibir_valores_fechamento_caixa, ce.usa_controle_caixa,ce.porcentagem_garcom,ce. permitir_acompanhar_status
+          ce.valor_inicial_caixa_padrao, ce.exibir_valores_fechamento_caixa, ce.usa_controle_caixa,ce.porcentagem_garcom,ce.permitir_acompanhar_status
        FROM empresas e
        LEFT JOIN config_empresa ce ON e.id = ce.empresa_id
        WHERE e.id = ?`,
